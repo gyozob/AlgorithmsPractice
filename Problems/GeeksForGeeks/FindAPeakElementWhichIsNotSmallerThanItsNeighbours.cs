ef604448baa1220f1295bfa72e2224eee1400395
Iosif Balla <iosif.balla@visma.com>
2023-07-19 13:33:25 +0300

    FindAPeakElementWhichIsNotSmallerThanItsNeighbours

---
+++ Problems/GeeksForGeeks/FindAPeakElementWhichIsNotSmallerThanItsNeighbours.cs
@@ -0,0 +1,29 @@
+﻿namespace Problems.GeeksForGeeks
+{
+    /// <summary>
+    /// Link to the problem https://www.geeksforgeeks.org/find-a-peak-in-a-given-array/
+    /// </summary>
+    public class FindAPeakElementWhichIsNotSmallerThanItsNeighbours
+    {
+        [Test]
+        public void Run() 
+        {
+            var input = new[] { 10, 20, 15, 2, 23, 90, 67 };
+            for (int i = 1; i < input.Length - 1; i++)
+            {
+                var isCurrentGreaterThanNext = input[i] > input[i + 1];
+                if (input[i] > input[i - 1] && isCurrentGreaterThanNext)
+                {
+                    Console.WriteLine(input[i]);
+                    continue;
+                }
+
+                // if so then the check in the next iteration doesn't make sense so we step over
+                if (isCurrentGreaterThanNext)
+                {
+                    i++;
+                }
+            }
+        }
+    }
+}
