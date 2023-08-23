e1b92feb507889ecce9e75f3b683213e3f26d8bc
Iosif Balla <iosif.balla@visma.com>
2023-08-23 11:01:00 +0300

    added a codility problem

---
+++ Problems/Codility/MissingInteger.cs
@@ -0,0 +1,28 @@
+﻿namespace Problems.Codility
+{
+    /// <summary>
+    /// Find the lowest positive integer greater than 0 which doesn't exists in array A with max 100000 elements
+    /// </summary>
+    public class MissingInteger
+    {
+        [Test]
+        [TestCase(new int[] { 1, 2, 3 })]
+        [TestCase(new int[] { -1, -2, 6, 5 })]
+        [TestCase(new int[] { -1, -2 })]
+        public void Run(int[] a)
+        {
+            var dictA = a.ToHashSet();
+            var minPositive = 0;
+            for (int i = 1; i <= 100001; i++)
+            {
+                if (!dictA.Contains(i))
+                {
+                    minPositive = i;
+                    break;
+                }
+            }
+
+            Assert.Greater(minPositive, 0);
+        }
+    }
+}
